6f73c4f682b006885ec94c16d09ced59092a037a
Suraj Sawant <suraj.sawant05@outlook.com>
2022-01-16 15:02:06 +0530

    first commit for all files

--- sunkart/settings.py
+++ sunkart/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'sunkart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
